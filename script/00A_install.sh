c8f8ae8049d152e14aa6278c634e51d792cb2981
klterwelp <klterwelp@gmail.com>
2023-06-01 11:27:04 -0400

    Add script info header and create new folders

--- script/00A_install.sh
+++ script/00A_install.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
+# ------------------ SCRIPT INFO: 00_install.sh -------------------- #
 
-# file: 00_install.sh
+# PURPOSE
+# - set up pipeline folders
+# - replace partition name and email address in .slurm scripts
+# INPUT
+# - script folder containing all scripts ".slurm"
+# - Set script/config.sh VARIABLES
+#   - $email_address
+#   - $part_name
+# OUTPUT
+# - make folders: rawData, cleanData, log, cache, output, report
+# - replace partition name and email address based on config.sh
 
-# making folders
-mkdir -p ./log
-mkdir -p ../meta
+# ----------------- SCRIPT START -------------------- # 
 
-# load parameters
+# making folders for analysis 
+mkdir -p ./{rawData,cleanData}/{data,meta}
+mkdir -p ./{log,cache,output,report}
+mkdir -p report/{graphs,docs}
+
+# load parameters from config file
 dos2unix ./config.sh
 source ./config.sh
 
 # create script log
 exec &>> ./log/00_install.log
 
-# Replacing working dir in all slurm files
+# replacing partition name in all slurm files
 sed -i "s/PART_NAME/${part_name}/g" *.slurm*
 
-# Replacing email address in all slurm files
+# replacing email address in all slurm files
 sed -i "s/EMAIL_ADDRESS/${email_address}/g" *.slurm*
 
